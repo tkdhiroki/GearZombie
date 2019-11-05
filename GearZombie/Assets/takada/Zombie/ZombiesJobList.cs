abe6b59729541df3153083414d029826d74050f7
a <a>
2019-11-05 18:50:54 +0900

    zombieをScriptableObjectで設定できるようにした

---
+++ GearZombie/Assets/takada/Zombie/ZombiesJobList.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ZombieJob
+{
+    normal,
+    Max
+}
+
+[CreateAssetMenu(menuName = "Zombie/Create ZombieTable", fileName = "ZombieTable")]
+public class ZombiesJobList : ScriptableObject
+{
+    public List<ZombieParam> ZombiesList = new List<ZombieParam>();
+}
+
+[System.Serializable]
+public struct ZombieParam
+{
+    public string Name;
+    public ZombieJob job;
+    public int HP, Attack, Deffence, Speed;
+    public float AttackSpeed;
+}
