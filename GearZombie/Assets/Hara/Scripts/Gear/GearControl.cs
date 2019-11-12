5a60a168a4bf251cb0cb19ba6faf889666b4e56c
Kazunori Hara <rize.game.0816@gmail.com>
2019-11-12 18:53:17 +0900

    [Update]歯車を回す処理

--- GearZombie/Assets/Hara/Scripts/Gear/GearControl.cs
+++ GearZombie/Assets/Hara/Scripts/Gear/GearControl.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GearControl : MonoBehaviour
 {
+    [SerializeField]
+    private GearView[] gearObject;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,29 @@ public class GearControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetMouseButton(0))
+        {
+            foreach(GearView gear in gearObject)
+            {
+                gear.RotateGearDirection = true;
+                gear.GearRotateFlag = true;
+            }
+        }
+        else if(Input.GetMouseButton(1))
+        {
+            foreach(GearView gear in gearObject)
+            {
+                gear.RotateGearDirection = false;
+                gear.GearRotateFlag = true;
+            }
+        }
+        else
+        {
+            foreach(GearView gear in gearObject)
+            {
+                gear.GearRotateFlag = false;
+            }
+        }
         
     }
 }
