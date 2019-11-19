80a20da2e102b16e53c38668564cae8091a37319
a <a>
2019-11-19 18:51:22 +0900

    zombie 画像、Jobの作成

---
+++ GearZombie/Assets/takada/Zombie/ZombieCondition.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Condition
+{
+    Slow = 0b_0001,
+    Fire = 0b_0010,
+}
+
+public class ZombieCondition : MonoBehaviour
+{
+    private Condition zombieCondition = 0b_0000;
+    private void Start()
+    {
+        zombieCondition = zombieCondition | Condition.Slow;
+        Debug.Log(zombieCondition);
+        zombieCondition = zombieCondition | Condition.Fire;
+        Debug.Log(zombieCondition);
+        Condition fire = zombieCondition & Condition.Fire;
+        if(fire == Condition.Fire)
+        {
+            Debug.Log("もえている");
+        }
+        zombieCondition = zombieCondition & ( ~Condition.Slow);
+        Debug.Log(zombieCondition);
+    }
+
+}
---
+++ GearZombie/Assets/takada/Zombie/ZombieScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieScript : MonoBehaviour
+{
+    [SerializeField] ZombiesJobList zombie = null;
+
+
+    private void Start()
+    {
+
+    }
+
+    /// <summary>
+    /// ダメージ処理
+    /// </summary>
+    /// <param name="damage">ダメージ量</param>
+    public void Damaged(int damage)
+    {
+        int hp = zombie.Hp - damage;
+        zombie.Hp = hp;
+    }
+
+    private void ZombieMove()
+    {
+
+        
+    }
+
+    private void Init()
+    {
+
+    }
+}
