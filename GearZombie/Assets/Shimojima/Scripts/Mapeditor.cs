1b2db3552392e8b8c24cce6734c8a66fa5025d56
shimojima <sigkalra8888@gmail.com>
2019-11-19 16:27:40 +0900

    [Update]設置したオブジェクトを削除できるように修正

--- GearZombie/Assets/Shimojima/Scripts/Mapeditor.cs
+++ GearZombie/Assets/Shimojima/Scripts/Mapeditor.cs
@@ -5,8 +5,10 @@ using UnityEngine.UI;
 
 public class Mapeditor : MonoBehaviour
 {
+    public Dropdown inputSize;
     public Sprite[] mapObj;
     public Image preview;
+    private GameObject root;
     public float moveValue;
     private int defaultSize = 7;
     public int objSize;
@@ -16,7 +18,8 @@ public class Mapeditor : MonoBehaviour
 
     void Start()
     {
-
+        root = new GameObject();
+        root.name = "Root";
     }
 
     // Update is called once per frame
@@ -30,6 +33,11 @@ public class Mapeditor : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Return))
         {
             MapObjSet(number);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Delete))
+        {
+            MapObjDestroy();
         }
     }
 
@@ -80,14 +88,15 @@ public class Mapeditor : MonoBehaviour
             return;
         }
 
+        int size = inputSize.value + 1;
         Vector3 pos = transform.position;
         GameObject obj = new GameObject();
         obj.name = "map";
         obj.AddComponent<SpriteRenderer>().sprite = mapObj[x];
-        obj.transform.localScale = new Vector3(defaultSize * objSize, defaultSize * objSize, 1);
-        if (objSize != 1)
+        obj.transform.localScale = new Vector3(defaultSize * size, defaultSize * size, 1);
+        if (size != 1)
         {
-            obj.transform.localPosition = new Vector3(pos.x + 0.5f * (objSize - 1), pos.y + 0.5f * (objSize - 1), pos.z);
+            obj.transform.localPosition = new Vector3(pos.x + 0.5f * (size - 1), pos.y + 0.5f * (size - 1), pos.z);
         }
         else
         {
@@ -95,7 +104,23 @@ public class Mapeditor : MonoBehaviour
         }
         Vector2 v = obj.AddComponent<BoxCollider2D>().size;
         obj.GetComponent<BoxCollider2D>().size = new Vector2(v.x - 0.03f, v.y - 0.03f);
+        obj.transform.parent = root.transform;
 
+    }
+
+    private void MapObjDestroy()
+    {
+        Collider2D[] col2d = new Collider2D[5];
+        gameObject.GetComponent<Collider2D>().OverlapCollider(new ContactFilter2D(), col2d);
+        foreach (var c in col2d)
+        {
+            if (c != null && c.name == "map")
+            {
+                Debug.Log(c.name + "オブジェクトを削除しました");
+                Destroy(c.gameObject);
+                break;
+            }
+        }
     }
 
     private void MapObjChange()
