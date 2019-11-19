a1668958ab6d5fea772e2d5c447f508828977dff
Kazunori Hara <rize.game.0816@gmail.com>
2019-11-19 18:27:15 +0900

    [Update]歯車の追加

--- GearZombie/Assets/Hara/Scripts/Gear/GearControl.cs
+++ GearZombie/Assets/Hara/Scripts/Gear/GearControl.cs
@@ -1,44 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class GearControl : MonoBehaviour
 {
     [SerializeField]
-    private GearView[] gearObject;
+    private GearView gearObject = null;
+
+    private EventTrigger trigger = null;
+    private bool gearFlag = false;
+
+    public void SetGearFlag(bool b)
+    {
+        gearFlag = b ? true : false;
+    }
+
+    private void Awake()
+    {
+        if(gearObject != null)
+        {
+            trigger = gearObject.GetComponent<EventTrigger>();
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(trigger != null)
+        {
+            EventTrigger.Entry pointerEnter = new EventTrigger.Entry();
+            pointerEnter.eventID = EventTriggerType.PointerEnter;
+            pointerEnter.callback.AddListener((x) => { gearFlag = true; });
+            trigger.triggers.Add(pointerEnter);
+
+            EventTrigger.Entry pointerExit = new EventTrigger.Entry();
+            pointerExit.eventID = EventTriggerType.PointerExit;
+            pointerExit.callback.AddListener((x) => { gearFlag = false; if(gearObject.GearRotateFlag) gearObject.GearRotateFlag = false; });
+            trigger.triggers.Add(pointerExit);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        MouseClick();
+    }
+
+    private void MouseClick()
+    {
+        if(!gearFlag) return;
+
         if(Input.GetMouseButton(0))
         {
-            foreach(GearView gear in gearObject)
-            {
-                gear.RotateGearDirection = true;
-                gear.GearRotateFlag = true;
-            }
+            gearObject.RotateGearDirection = true;
+            gearObject.GearRotateFlag = true;
         }
         else if(Input.GetMouseButton(1))
         {
-            foreach(GearView gear in gearObject)
-            {
-                gear.RotateGearDirection = false;
-                gear.GearRotateFlag = true;
-            }
+            gearObject.RotateGearDirection = false;
+            gearObject.GearRotateFlag = true;
         }
         else
         {
-            foreach(GearView gear in gearObject)
-            {
-                gear.GearRotateFlag = false;
-            }
+            gearObject.GearRotateFlag = false;
         }
-        
     }
 }
--- GearZombie/Assets/Hara/Scripts/Gear/GearView.cs
+++ GearZombie/Assets/Hara/Scripts/Gear/GearView.cs
@@ -5,9 +5,14 @@ using UnityEngine.UI;
 
 public class GearView : MonoBehaviour
 {
-    public bool GearRotateFlag { set; private get; } = false;
+    public bool GearRotateFlag { set; get; } = false;
     public bool RotateGearDirection { set; private get; } = false;
-    
+
+    private GameObject gearObj1 = null;
+    private GameObject gearObj2 = null;
+    private GameObject gearObj3 = null;
+    private GameObject gearObj4 = null;
+    private GameObject gearObj5 = null;
 
     [SerializeField, Tooltip("歯車が1秒間で回転する角度")]
     private float angle = 180;
@@ -15,7 +20,7 @@ public class GearView : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetGearObject();
     }
 
     // Update is called once per frame
@@ -40,6 +45,38 @@ public class GearView : MonoBehaviour
         {
             direct = 1;
         }
-        transform.RotateAround(transform.position, Vector3.forward, direct * (angle * Time.deltaTime));
+
+        if(gearObj1 != null)
+        {
+            gearObj1.transform.RotateAround(gearObj1.transform.position, Vector3.forward, direct * (angle * Time.deltaTime));
+        }
+        if(gearObj2 != null)
+        {
+            gearObj2.transform.RotateAround(gearObj2.transform.position, Vector3.forward, -direct * (angle * Time.deltaTime));
+        }
+        if(gearObj3 != null)
+        {
+            gearObj3.transform.RotateAround(gearObj3.transform.position, Vector3.forward, direct * (angle * Time.deltaTime));
+        }
+        if(gearObj4 != null)
+        {
+            gearObj4.transform.RotateAround(gearObj4.transform.position, Vector3.forward, -direct * (angle * Time.deltaTime));
+        }
+        if(gearObj5 != null)
+        {
+            gearObj5.transform.RotateAround(gearObj5.transform.position, Vector3.forward, direct * (angle * Time.deltaTime));
+        }
+    }
+
+    /// <summary>
+    /// ギアオブジェクトの割り当て
+    /// </summary>
+    private void SetGearObject()
+    {
+        gearObj1 = transform.GetChild(0).gameObject;
+        gearObj2 = transform.GetChild(1).gameObject;
+        gearObj3 = transform.GetChild(2).gameObject;
+        gearObj4 = transform.GetChild(3).gameObject;
+        gearObj5 = transform.GetChild(4).gameObject;
     }
 }
