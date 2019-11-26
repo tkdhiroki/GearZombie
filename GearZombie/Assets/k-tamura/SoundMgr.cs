10a7cf5c63ed5710e498965a18ffc3b9ba688f28
tamu <kuro@kuro.games>
2019-11-26 17:13:39 +0900

    簡単にサウンドマネージャー実装

---
+++ GearZombie/Assets/k-tamura/SoundMgr.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundMgr :SingletonMonoBehaviour<SoundMgr>
+{
+    [SerializeField]
+    AudioSource[] audioSource;
+    [SerializeField]
+    AudioClip[] audioClip;
+    public static void BgmPlay(int i)
+    {
+        instance.audioSource[0].clip = instance.audioClip[i];
+        instance.audioSource[0].loop = true;
+        instance.audioSource[0].Play();
+
+    }
+
+    public static void SeShot(int soundNum)
+    {
+        for(var i=1;i<=instance.audioSource.Length;i++)
+        {
+            if(!instance.audioSource[i].isPlaying)
+            {
+                instance.audioSource[i].PlayOneShot(instance.audioClip[soundNum]);
+                return;
+            }
+        }
+    }
+}
+class SoundData
+{
+
+}
