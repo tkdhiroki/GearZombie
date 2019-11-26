f2a673c606c7803587fa3ecb1deffb7400d6b25c
KURO-Games <kuro@kuro.games>
2019-11-26 16:47:10 +0900

    komitto

---
+++ GearZombie/Assets/k-tamura/GearStatus/TrapStatus.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Security;
+[CreateAssetMenu(menuName = "Trap/TrapStatus", fileName = "TrapStat")]
+public class TrapStatus : ScriptableObject
+{
+    public TrapStat trapstatus;
+    
+}
+[System.Serializable]
+public struct TrapStat
+{
+    [SerializeField, Header("TrapName")] public string _trapName;
+    public string TrapName { get { return _trapName; } }
+
+    [SerializeField, Header("Trap生成Num")] public int _trapNum;
+    public int TrapNumber { get { return _trapNum; } }
+
+    [SerializeField, Header("耐久値")] public int _endurance;
+    public int Endurance { get { return _endurance; } }
+}
