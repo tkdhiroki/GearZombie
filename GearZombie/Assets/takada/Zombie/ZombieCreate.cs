6b345f1cfd168323ed933bb2769a87e1f698caad
fujita <>
2019-12-16 18:45:51 +0900

    aaaaa

--- GearZombie/Assets/takada/Zombie/ZombieCreate.cs
+++ GearZombie/Assets/takada/Zombie/ZombieCreate.cs
@@ -4,16 +4,39 @@ using UnityEngine;
 
 public class ZombieCreate : MonoBehaviour
 {
-    [SerializeField] MapSetting mapSetting = null;
-    private GameObject zombieBox;
+    [SerializeField] private MapSetting mapSetting = null;
+    private Transform zombieBox;
+    private List<ZombieSpawnClass> zombieSpawn = new List<ZombieSpawnClass>();
 
     private void Start()
     {
-        zombieBox = this.gameObject.gameObject;
+        zombieBox = this.transform;
+        zombieSpawn = mapSetting.spawnClasses;
+
+        // create zombie ( active => false )
+        FieldZombieInit();
     }
 
     private void FieldZombieInit()
     {
-        //for(int i = 0; i < )
+        // count check
+        if (zombieSpawn.Count < 1) return;
+
+        // １マップ分のエネミー生成
+        for (int listNum = 0; listNum < zombieSpawn.Count; listNum++)
+        {
+            for(int spawn = 0;spawn < zombieSpawn[listNum].spawnNum; spawn++)
+            {
+                var obj = Instantiate(zombieSpawn[listNum].zombiePrefab, zombieBox);
+                obj.SetActive(false);
+            }
+        }
+        #if UNITY_EDITOR
+        Debug.Log(zombieBox.childCount);
+        #endif
+    }
+    private void SpawnZombie()
+    {
+        
     }
 }
