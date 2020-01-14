cc8a12e46c2f41b1280ba56d444486b8e90b59b3
KURO-Games <kuro@kuro.games>
2020-01-14 17:40:12 +0900

    タイトル素材制作その他。

---
+++ GearZombie/Assets/k-tamura/ZonbiePop.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZonbiePop : MonoBehaviour
+{
+	float _time;
+    GameObject zonbie;
+    GameObject PopPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _time += Time.deltaTime;
+
+    }
+    void Pop()
+    {
+        Instantiate(zonbie,PopPoint.transform.position,Quaternion.identity);
+    }
+}
