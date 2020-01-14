a93ed8159a2400a21774a013a6aad75e2964d537
shimojima <sigkalra8888@gmail.com>
2020-01-14 17:22:50 +0900

    [Update]Mapを選択しているか分かりやすくする調整

--- GearZombie/Assets/Shimojima/Scripts/MapController.cs
+++ GearZombie/Assets/Shimojima/Scripts/MapController.cs
@@ -4,64 +4,8 @@ using UnityEngine;
 
 public class MapController : MonoBehaviour
 {
-    private bool isTarget = false;
-    private bool isMax = true;
-    private bool firstTime = false;
-    private float alpha = 1;
-    private Color c;
-
-    void Start()
-    {
-        c = gameObject.GetComponent<SpriteRenderer>().color;
-    }
-    
-    void Update()
-    {
-        ColorFade();
-    }
-
-    private void ColorFade()
-    {
-        if (isTarget)
-        {
-            if (!firstTime)
-            {
-                c = Color.yellow;
-                firstTime = true;
-            }
-
-            if (alpha > 0 && isMax)
-            {
-                gameObject.GetComponent<SpriteRenderer>().color = new Color(c.r, c.g, c.b, alpha);
-                alpha -= 0.03f;
-            }
-            else if (alpha <= 0)
-            {
-                isMax = false;
-            }
-
-            if (alpha < 1 && !isMax)
-            {
-                gameObject.GetComponent<SpriteRenderer>().color = new Color(c.r, c.g, c.b, alpha);
-                alpha += 0.03f;
-            }
-            else if (alpha >= 1)
-            {
-                isMax = true;
-            }
-
-        }
-    }
-
     private void OnMouseDown()
     {
-        if (!isTarget)
-        {
-            isTarget = true;
-        }
-        else
-        {
-            isTarget = false;
-        }
+        MapManager.IsChoice(gameObject);
     }
 }
