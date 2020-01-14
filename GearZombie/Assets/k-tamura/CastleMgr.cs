90c0942dc896d95d43290762c616efc0bb2b571b
aaa <a@a>
2020-01-14 18:27:05 +0900

    GameOver処理追加

--- GearZombie/Assets/k-tamura/CastleMgr.cs
+++ GearZombie/Assets/k-tamura/CastleMgr.cs
@@ -12,14 +12,33 @@ public class CastleMgr : SingletonMonoBehaviour<CastleMgr>
     private Image GaugeUi;
     [SerializeField]
     private float HpGauge;
+    [SerializeField]
+    private Text GameoverText;
+    private float fadeSpeed = 2.5f;
+
     private float nowHp;
     private void Start()
     {
         nowHp = HpGauge;
+
+        Color color = new Color();
+        color.a = 0;
+
+        GameoverText.color = color;
     }
     public static void HpDecrease(float HpDec)
     {
         instance.nowHp -= HpDec;
         instance.GaugeUi.fillAmount = instance.nowHp / instance.HpGauge;
+
+        if(instance.nowHp <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    private static void GameOver()
+    {
+        instance.GameoverText.DOFade(1.0f, instance.fadeSpeed);
     }
 }
