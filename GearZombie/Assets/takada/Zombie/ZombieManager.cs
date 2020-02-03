17513231f1cfdc2cfe7f4484ca2541898e47a37c
tokyotech-hosyu <tokyotech-hosyu@4900.co.jp>
2020-02-03 15:13:21 +0900

    a

---
+++ GearZombie/Assets/takada/Zombie/ZombieManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*------------------------------------------
+ * Waveを管理するクラス
+ * 現在のWave、 Waveが終わったかどうか、 Waveで出現するオブジェクトの作成
+--------------------------------------------*/
+
+
+public class ZombieManager : SingletonMonoBehaviour<ZombieManager>
+{
+
+}
